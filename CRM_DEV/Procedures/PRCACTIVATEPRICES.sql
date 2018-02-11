5bcb1b8740d349249ecf5da18a5a6fcd4fc6cb88
randvere <chris.randvere@red-gate.com>
2018-02-11 20:13:52 +0000

    test

--- CRM_DEV/Procedures/PRCACTIVATEPRICES.sql
+++ CRM_DEV/Procedures/PRCACTIVATEPRICES.sql
@@ -2,6 +2,6 @@ CREATE OR REPLACE PROCEDURE crm_dev.prcActivatePrices
 IS
 BEGIN
   UPDATE crm_dev.WidgetPrices SET Active='N' WHERE sysdate<DateValidTo OR sysdate>DateValidFrom;
-  UPDATE crm_dev.WidgetPrices SET Active='Y' WHERE sysdate>=DateValidFrom AND sysdate<=DateValidFrom;
+  UPDATE crm_dev.WidgetPrices SET Active='Y' WHERE sysdate>=DateValidFrom OR sysdate<=DateValidFrom;
 END;
 /
